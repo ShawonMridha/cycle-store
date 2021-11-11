4be60edaa0d8e1eab95b31b348497e07ce9bcd25
ShawonMridha <you@example.com>
2021-11-11 17:41:48 +0600

    again push

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAXm2tmi-WdhNm-axDi8bUY9TMwFWwXsyA",
-    authDomain: "cycle-shop-b0b72.firebaseapp.com",
-    projectId: "cycle-shop-b0b72",
-    storageBucket: "cycle-shop-b0b72.appspot.com",
-    messagingSenderId: "282296162579",
-    appId: "1:282296162579:web:beb04aeed1e87e27b5e2e6"
-  };
+  apiKey: "AIzaSyAXm2tmi-WdhNm-axDi8bUY9TMwFWwXsyA",
+  authDomain: "cycle-shop-b0b72.firebaseapp.com",
+  projectId: "cycle-shop-b0b72",
+  storageBucket: "cycle-shop-b0b72.appspot.com",
+  messagingSenderId: "282296162579",
+  appId: "1:282296162579:web:beb04aeed1e87e27b5e2e6"
+};
 
 export default firebaseConfig;
